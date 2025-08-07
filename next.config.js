d3b19c39e820b9037565f336c977acdb96a916a6
Ayesha Nahman <154921970+AyeshaNahman@users.noreply.github.com>
2025-08-07 21:12:54 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'luosmszmmdhxucctpzvl.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/images/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
